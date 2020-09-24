e31d163ebe635ecdfccff63cf4ec766b6c76a5eb
Amaro Terrazas <42896111+AmaroT@users.noreply.github.com>
2020-09-23 22:18:56 -0500

    created exercise 1-5, completed 1-4

--- src/main/java/Java101.java
+++ src/main/java/Java101.java
@@ -1,7 +1,33 @@
 package main.java;
 
+import java.util.ArrayList;
+
 public class Java101 {
-    public static void main(String[] args) {
-        System.out.println("Hello, world");
+//    public static void main(String[] args) {
+//        System.out.println("Hello, world");
+//    }
+    public static boolean isGreaterThan007(double a, double b) {
+        return(a > b);
     }
+    //Exercise 2
+    //Create a variable named fruits and assign it to an array of strings containing the following fruits.
+    // mango, banana, guava, kiwi, strawberry
+    String[] fruits = {"mango", "banana", "guava", "kiwi", "strawberry"};
+
+    //Exercise 3
+    //Create a var name vegatable and assign it to an array of fruits containing the following vegatable names as strings:
+    //eggplant, broccoli, cauliflower, and zucchini
+
+    String[] vegetables = {"eggplant", "broccoli", "carrot", "cauliflower", "zucchini"};
+
+    //Exercise 4
+    //Create a variable named numbers and assign it to an array of numbers: 1,2,3,4,5,6,7,8,9,10
+    String[] numbers = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+
+    //Exercise 5
+    // Add a string "tomato" to the end of the fruits array.
+    //*Hint* Recommend finding and using a built-in JS operations to add to an array rather than recreating the array.
+    //*Hint* in Java, find and use a different way to work around this...
+
+
 }
