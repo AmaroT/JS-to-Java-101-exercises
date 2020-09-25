a001a5d310db5b3a51494cff115588de5fcdbc39
Amaro Terrazas <42896111+AmaroT@users.noreply.github.com>
2020-09-25 12:36:14 -0500

    completed 5-10 exercises

--- src/main/java/Java101.java
+++ src/main/java/Java101.java
@@ -1,6 +1,8 @@
 package main.java;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class Java101 {
 //    public static void main(String[] args) {
@@ -13,22 +15,103 @@ public class Java101 {
     //Exercise 2
     //Create a variable named fruits and assign it to an array of strings containing the following fruits.
     // mango, banana, guava, kiwi, strawberry
-    String[] fruits = {"mango", "banana", "guava", "kiwi", "strawberry"};
+//    String[] fruits = new String[] {"mango", "banana", "guava", "kiwi", "strawberry"};
 
     //Exercise 3
     //Create a var name vegatable and assign it to an array of fruits containing the following vegatable names as strings:
     //eggplant, broccoli, cauliflower, and zucchini
 
-    String[] vegetables = {"eggplant", "broccoli", "carrot", "cauliflower", "zucchini"};
+//    String[] vegetables = new String[] {"eggplant", "broccoli", "carrot", "cauliflower", "zucchini"};
+
 
     //Exercise 4
     //Create a variable named numbers and assign it to an array of numbers: 1,2,3,4,5,6,7,8,9,10
-    String[] numbers = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+//    String[] numbers =  new String[] {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
 
     //Exercise 5
     // Add a string "tomato" to the end of the fruits array.
     //*Hint* Recommend finding and using a built-in JS operations to add to an array rather than recreating the array.
     //*Hint* in Java, find and use a different way to work around this...
 
+//    ArrayList<String> mFruits = new ArrayList<String>();
+//    mFruits.add("mango");
+//    mFruits.add("banana");
+//    mFruits.add("guava");
+//    mFruits.add("kiwi");
+//    mFruits.add("strawberry");
+//    mFruits.add("tomato");
 
-}
+    //Exercise 6 :
+    //add the string tomato onto the end of vegatables array.
+//    ArrayList<String> mVegetables = new ArrayList<String>();
+//    mVegetables.add("eggplant");
+//    mVegetables.add("broccoli");
+//    mVegetables.add("carrot");
+//    mVegetables.add("cauliflower");
+//    mVegetables.add("zucchini");
+//    mVegetables.add("tomato");
+
+    //Exercise 7:
+    //Given the array of numbers defind below, reverse the array of numbers that you created above.
+//    public static void main (String[] args){
+//        ArrayList obj = new ArrayList();
+//        //Declaring arraylist without any initial size
+//        ArrayList<Integer> mNumbers = new ArrayList<Integer>();
+//
+//        //Appending elements at the end of the list
+//        mNumbers.add(1);
+//        mNumbers.add(2);
+//        mNumbers.add(3);
+//        mNumbers.add(4);
+//        mNumbers.add(5);
+//        mNumbers.add(6);
+//        mNumbers.add(7);
+//        mNumbers.add(8);
+//        mNumbers.add(9);
+//        mNumbers.add(10);
+//        System.out.println("Elements before reversing: " + mNumbers);
+//        Collections.reverse(mNumbers);
+//        System.out.println("Elements after reversing: " + mNumbers);
+//
+//    }
+    //Exercise 8:
+    // Sort the Vegetables in alphabetical order.
+//    public static void main(String[] args) {
+//        String[] strArr = new String[] {"broccoli", "carrot", "cauliflower", "eggplant", "eggplant", "tomato","zucchini"};
+//        System.out.println("Sorting the array in alphabetical order...");
+//        Arrays.sort(strArr, String.CASE_INSENSITIVE_ORDER);
+//        for(int a = 0; a < strArr.length; a++){
+//            System.out.println(strArr[a]);
+//        }
+    //Exercise 9 :
+    //Write the code necessary to sort the fruits in reverse alphabetical order
+//    public static void main(String[]args){
+//        String[] strArr = new String[] {"mango","banana", "guava", "kiwi", "strawberry"};
+//        System.out.println("Sorting the array in reverse alphabetical order...");
+//        Arrays.sort(strArr, Collections.reverseOrder());
+//        for(int a = 0; a < strArr.length; a++){
+//            System.out.println(strArr[a]);
+//        }
+    //Exercise 10:
+    // Write the code necessary to produce a single array that hold all fruits then all vegetables in the order as they were sorted above.
+
+//    public static void main(String[] args) throws Exception{
+//        ArrayList<String> fruits = new ArrayList<>(Arrays.asList("mango", "banana", "guava", "kiwi", "strawberry"));
+//        ArrayList<String> vegetables = new ArrayList<>(Arrays.asList("eggplant", "broccoli", "carrot", "cauliflower", "zucchini"));
+//
+//        fruits.addAll(vegetables); //Merging both lists
+//        System.out.println(fruits);
+//    }
+
+
+
+
+    }
+
+
+
+
+
+
+
+
