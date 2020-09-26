4e6b26eba2e462d82097646609a7214085848c22
Amaro Terrazas <42896111+AmaroT@users.noreply.github.com>
2020-09-26 17:40:43 -0500

    fizzbuzz practice and repetition for Java skill building

--- src/main/java/Java101.java
+++ src/main/java/Java101.java
@@ -238,6 +238,75 @@ public class Java101 {
 //            System.out.println("The integer is not a negative and not even");
 //        }
 //    }
+    //fizzbuzz 1
+//    public static void main(String[] args) {
+//        int n = 100;
+//        //loop for 100 times
+//        for(int i = 1; i <=n; i++){
+//            if(i % 15 == 0){
+//                System.out.println("Fizzbuzz" + " ");
+//                //number divisible by 5, print 'Buzz'
+//                //in place of the number
+//            } else if (i % 5 == 0){
+//                System.out.println("Buzz" + " ");
+//
+//                //number divisible by 3, print 'Fizz'
+//                //in place of number
+//            } else if (i % 3 == 0){
+//                System.out.println("Fizz" + " ");
+//
+//                //number divisible by 15(divisible by
+//                //by both 3 & 5), print 'FizzBuzz' in
+//                //place of the number
+//            } else {
+//                System.out.println(i + " ");
+//            }
+//        }
+//    }
+    //Fizzbuzz 2
+//    public static void main(String[] args) {
+//        int n = 100;
+//        //loop for 100 times
+//        for(int i = 1;i <= n;i++){
+//            //if divisible by both 3 && 5
+//            if(i % 15 == 0) {
+//                System.out.println("Fizzbuzz");
+//
+//                //if divisible by 5
+//            }else if(i % 5 == 0){
+//                    System.out.println("Buzz");
+//
+//                    //if divisible by 3
+//            } else if(i % 3 == 0) {
+//                System.out.println("Fizz");
+//            } else{
+//                System.out.println(i);
+//            }
+//        }
+//    }
+    //Fizzbuzz 3
+    public static void main(String[] args) {
+        int n = 100;
+        //loop for 100 times
+        for(int i = 1; i <= n;i++){
+            //if divisible by 15
+            if(i % 15 == 0){
+                System.out.println("FizzBuzz");
+
+                //If divisible by 3
+            } else if(i % 3 == 0){
+                System.out.println("Fizz");
+
+                // if divisible by 5
+            } else if(i % 5 == 0){
+                System.out.println("Buzz");
+            } else{
+                System.out.println(i);
+            }
+
+        }
+    }
+
 }
 
 
